482f3d610e0ace9c11f14331bb878406f246e352
LevanceWam <lkwamley@fullsail.edu>
2020-04-17 11:17:53 -0400

    finished numbers

---
+++ PrimitveTypes/Numbers.py
@@ -0,0 +1,14 @@
+x = 1  # integers
+x = 1.1  # floats
+x = 1 + 2j  # a + bi, complex numbers not used in web dev
+print(10 + 3)  # addition
+print(10 - 3)  # subtraction
+print(10 * 3)  # multiply
+print(10 / 3)  # division that gives you a float
+print(10 // 3)  # division that gives you interger
+print(10 % 3)  # modelus that gives you the remaining value of the division
+print(10 ** 3)  # exponent
+
+y = 10
+y = y + 3  # same as line 14
+y += 3  # augmented expression
