54e929c74927dac40719694146368bc57a8e70dd
LevanceWam <lkwamley@fullsail.edu>
2020-04-19 13:36:15 -0400

    Finished a video on list

---
+++ DataStuctures/List.py
@@ -0,0 +1,8 @@
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 4]]  # A 2 dimensional list
+zeros = [0] * 5  # we can repeat the items in a list
+combined = zeros + letters
+print(combined)  # concatinating list
+numbers = list(range(20))  # Creates a list from 0-20
+chars = list("Hello World")  # will give each character their own index
+print(len(chars))
