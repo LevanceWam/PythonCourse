5195c89b71802560af090d3e4dc583f971f99edf
LevanceWam <lkwamley@fullsail.edu>
2020-04-19 17:44:12 -0400

    finished a video on zip functions

---
+++ DataStuctures/ZipFunctions.py
@@ -0,0 +1,7 @@
+list1 = [1, 2, 3]
+list2 = [10, 20, 30]
+
+# the zip function takes multiple iterables
+print(list(zip("abc", list1, list2)))
+# The zip function combined the 2 list together
+# we can also combine a string too
