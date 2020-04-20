9b585b58ebb8bc679a28318265a74823cd5d11e3
LevanceWam <lkwamley@fullsail.edu>
2020-04-20 15:27:49 -0400

    finished a video on arrays

---
+++ DataStuctures/Arrays.py
@@ -0,0 +1,10 @@
+from array import array
+# arrays share a lot of the same methods as list
+# append
+# remove
+# insert
+# pop
+numbers = array("i", [1, 2, 3])
+# unlike list the objects in the array are typed
+# so if we put a float or anything that is not the same type ot will give a error
+# Only use arrays when you have a large sequence of numbers and sre experiecing issues
