0a0d178f4b5a00e18ca6556ef9e525f7f3f00f9b
LevanceWam <lkwamley@fullsail.edu>
2020-04-20 15:13:01 -0400

    Watched a video on switching variables

---
+++ DataStuctures/SwappingVariables.py
@@ -0,0 +1,21 @@
+x = 10
+y = 11
+
+# z = x
+# x = y
+# y = z
+
+# this is a tuple
+# we are defining a tuple and then unpacking it
+# same as lines 4-6
+x, y = y, x
+
+# Visual to make it easier
+# unpacking
+a, b = (1, 2)
+print("a", a)
+print("b", b)
+
+
+print("x", x)
+print("y", y)
