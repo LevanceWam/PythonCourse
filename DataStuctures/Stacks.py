1f6c2fd387deafd7342da3473b27f09a97d2fdf7
LevanceWam <lkwamley@fullsail.edu>
2020-04-20 14:37:08 -0400

    finished the video on stacks

---
+++ DataStuctures/Stacks.py
@@ -0,0 +1,10 @@
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+print(browsing_session)
+last = browsing_session.pop()
+print(last)
+print(browsing_session)
+if not browsing_session:
+    print("Redirect", browsing_session[-1])
