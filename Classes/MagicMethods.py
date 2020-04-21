71c34c25cb96a0da653f92c7a0054e9fc7c4cefe
LevanceWam <lkwamley@fullsail.edu>
2020-04-21 12:03:02 -0400

    Finished a video on magic methods

---
+++ Classes/MagicMethods.py
@@ -0,0 +1,15 @@
+class Point:
+    # magic method
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}), ({self.y})"
+
+    def draw(self):
+        print(f"Point ({self.x}), ({self.y})")
+
+
+point = Point(1, 2)
+print(point)
