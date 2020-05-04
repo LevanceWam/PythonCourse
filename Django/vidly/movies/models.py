77495b5689590346e4a973c7b4201eb278e2d14c
LevanceWam <lkwamley@fullsail.edu>
2020-05-04 14:44:08 -0400

    Created models for the movies

--- Django/vidly/movies/models.py
+++ Django/vidly/movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    number_in_stock = models.IntegerField()
+    daily_rate = models.FloatField()
+    # with this if the genre is deleted then all the moves under that genre is deleted as well
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
