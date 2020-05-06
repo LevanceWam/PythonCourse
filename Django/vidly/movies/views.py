84a0614b3a7684348b0aed5ea8baf6105b78e301
LevanceWam <lkwamley@fullsail.edu>
2020-05-06 15:22:11 -0400

    made it easier to get to the details page from the index.

--- Django/vidly/movies/views.py
+++ Django/vidly/movies/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 
@@ -9,5 +9,5 @@ def index(request):
 
 
 def detail(request, movie_id):
-    movie = Movie.objects.get(pk=movie_id)
+    movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'movies/detail.html', {'movie': movie})
