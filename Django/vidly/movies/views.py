ebbaf1457f8414bb816ce5a44c751848eff852c2
LevanceWam <lkwamley@fullsail.edu>
2020-05-06 13:57:50 -0400

    Added a template to the project

--- Django/vidly/movies/views.py
+++ Django/vidly/movies/views.py
@@ -7,5 +7,4 @@ from .models import Movie
 
 def index(request):
     movies = Movie.objects.all()
-    output = ', '.join([m.title for m in movies])
-    return HttpResponse(output)
+    return render(request, 'movies/index.html', {'movies': movies})
