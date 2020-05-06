d102387d80011ff419138679b4c9730bd703d6d1
LevanceWam <lkwamley@fullsail.edu>
2020-05-06 14:46:51 -0400

    added more urls

--- Django/vidly/movies/urls.py
+++ Django/vidly/movies/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:movie_id>', views.detail, name='movies_detail')
 ]
--- Django/vidly/movies/views.py
+++ Django/vidly/movies/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Movie
 
-# Create your views here.
-
 
 def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    return HttpResponse(movie_id)
