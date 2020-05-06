1ecaee4cdfa91f157e874a0a47b494c1bf6c3711
LevanceWam <lkwamley@fullsail.edu>
2020-05-06 13:35:36 -0400

    using the database api we pulled the movies from out of the database and put it on the web page

--- Django/vidly/movies/views.py
+++ Django/vidly/movies/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    movies = Movie.objects.all()
+    output = ', '.join([m.title for m in movies])
+    return HttpResponse(output)
