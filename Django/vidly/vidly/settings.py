bd2df758140ded832cf0c44b4a1a04e7ae29ccc2
LevanceWam <lkwamley@fullsail.edu>
2020-05-06 20:09:32 -0400

    Created a api fot the project

--- Django/vidly/vidly/settings.py
+++ Django/vidly/vidly/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "movies.apps.MoviesConfig",
+    "api.apps.ApiConfig"
 ]
 
 MIDDLEWARE = [
--- Django/vidly/vidly/urls.py
+++ Django/vidly/vidly/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.models import MovieResource
+
+movie_resource = MovieResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('movies.urls'))
+    path('movies/', include('movies.urls')),
+    path('api/', include(movie_resource.urls))
 ]
