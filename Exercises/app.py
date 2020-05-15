b5e3aa642ff63998e9381f38d616e0ba2e655fe1
LevanceWam <lkwamley@fullsail.edu>
2020-05-15 12:33:31 -0400

    Went back to the capital letter counter and expanded on it and added comments

--- Exercises/app.py
+++ Exercises/app.py
@@ -1,22 +1,30 @@
 # Let's write a program that will count the amount of capital letters in a file
+# By Levance Wamley
 import random
 import string
 
+# First select the file we want to read
 file = open('sample.txt')
 data = file.read()
+# returning the words in a list
 words = data.split()
 capitals = 0
 lowers = 0
 
+# Here we are going to iterate through the list and seperate the words
 for word in words:
-    if word.isupper():
-        capitals += 1
-    else:
-        lowers += 1
+    # Now we are iterating through the word to get the letter in each word
+    for letter in word:
+        # if the letter is capital then we will count it if not then we'll give a point to the lowercase counter
+        if letter.isupper():
+            capitals += 1
+        else:
+            lowers += 1
 
 print(f"There are {capitals} captial letters in the file.")
 print(f"There are {lowers} lowercase letters in the file.")
 
+# practice to get the understand how to get the amount of letters in a string
 cap_letters = 0
 low_letters = 0
 sentence = "".join(random.choices(string.ascii_letters, k=20))
