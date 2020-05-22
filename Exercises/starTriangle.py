5a609f72bec8691f23ad8952e7da3f26c78f9211
LevanceWam <lkwamley@fullsail.edu>
2020-05-21 23:28:56 -0400

    working on a different triangle

--- Exercises/starTriangle.py
+++ Exercises/starTriangle.py
@@ -2,16 +2,16 @@
 # by Levance Wamley
 
 # we are setting the amount of rows we want to have
-row = 8
-# This is the for loop that controls the rows being made.
+rows = 8
+# This is the for loop that controls the rowss being made.
 
 
-def triangle(row):
-    for x in range(0, row):
+def triangle(rows):
+    for x in range(0, rows):
         # So here we are making the columns this
         # Depending on which iteration it is on it will add the appropriate amount of spaces
         # Example 6 - 0 - 1 will give use 5 spaces and 1 star
-        for y in range(0, row-x-1):
+        for y in range(0, rows-x-1):
             print(end=" ")
             # With this loop we will print the star then a space to seperate them
             # Depending on the iteration we will get a amount of stars
@@ -21,27 +21,32 @@ def triangle(row):
         print()
 
 
-def reverseTriangle(row):
-    for i in range(row, 0, -1):
-        print(" "*(row-i)+"* "*(i))
+def reverseTriangle(rows):
+    for i in range(rows, 0, -1):
+        print(" "*(rows-i)+"* "*(i))
 
 
-def leftTopAngleTriangle(row):
-    for i in range(row, 0, -1):
+def leftTopTriangle(rows):
+    for i in range(rows, 0, -1):
         print("* " * i)
 
 
-def nintyDegree(row):
-    for i in range(row+1):
+def nintyDegree(rows):
+    for i in range(rows+1):
         print('* ' * i)
 
 
-# leftTopAngleTriangle(row)
-# nintyDegree(row)
+# def rightTopTriangle(rows):
+#     for i in range(0, rows):
+#         for col in range(0, rows):
+
+#             # leftTopAngleTriangle(rows)
+#             # nintyDegree(rows)
+
 
 def mixedTriangle():
-    leftTopAngleTriangle(row)
-    nintyDegree(row)
+    leftTopTriangle(rows)
+    nintyDegree(rows)
 
 
-mixedTriangle()
+# rightTopTriangle(rows)
