2de8c93d32cbad6e25fefc0eede89a51bbd96b4f
LevanceWam <lkwamley@fullsail.edu>
2020-05-29 16:04:23 -0400

    completed exercise 4

--- Exercises/exercise4.py
+++ Exercises/exercise4.py
@@ -1,1 +1,17 @@
 # return the total count of emma appears in a string
+# By Levance Wamley Jr
+
+sentence = 'Emma is a good developer. Emma is a writer. emma emma Emma.'
+
+
+def findWord(sentence, word):
+    count = 0
+    data = sentence.lower().replace(".", "").split()
+    for item in data:
+        if item == word.lower():
+            count += 1
+
+    print(count)
+
+
+findWord(sentence, 'Emma')
