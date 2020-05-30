8bcbfe8ede7d4da6b55d893e7b8b663ca9c5e080
LevanceWam <lkwamley@fullsail.edu>
2020-05-30 09:13:18 -0400

    Completed my 5th exercise

---
+++ Exercises/exercise5.py
@@ -0,0 +1,22 @@
+# Given two list of numbers
+# create a new list that will
+# take the odd numbers from the first list
+# take even numbers from the second list
+
+firstList = [11, 23, 44, 67, 232, 4, 32, 21]
+secondList = [22, 32, 456, 3, 45, 11, 55, 87, 98]
+result = []
+
+
+def thirdList(list1, list2):
+    for num in list1:
+        if num % 2 == 1:
+            result.append(num)
+
+    for num in list2:
+        if num % 2 == 0:
+            result.append(num)
+    print(result)
+
+
+thirdList(firstList, secondList)
