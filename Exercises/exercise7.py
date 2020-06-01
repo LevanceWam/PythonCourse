f3b20889a5d233426655b878b6b4b757f0bc86a2
LevanceWam <lkwamley@fullsail.edu>
2020-05-31 20:44:54 -0400

    completed another exercise

---
+++ Exercises/exercise7.py
@@ -0,0 +1,7 @@
+# create a right triangle filled with numbers
+# By Levance Wamley
+
+for row in range(5):
+    for col in range(row):
+        print(row, end=" ")
+    print()  # will cause a line break
