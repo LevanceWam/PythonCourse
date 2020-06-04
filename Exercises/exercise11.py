51bac95aab875b74aa302debb75f0da4e013fa71
LevanceWam <lkwamley@fullsail.edu>
2020-06-04 17:52:31 -0400

    Completed exercise 11

---
+++ Exercises/exercise11.py
@@ -0,0 +1,10 @@
+# create a function that takes a decimal/float and shows only
+# 2 numbers after the decimal
+# by Levance Wamley
+
+
+def shortDecimal(n):
+    print('%.2f' % (n))
+
+
+shortDecimal(3.14159)
