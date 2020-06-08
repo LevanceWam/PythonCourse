0fb7e8894816a789af823d0d745e1b5189e71dda
LevanceWam <lkwamley@fullsail.edu>
2020-06-08 10:41:28 -0400

    completed a short exercise

---
+++ Exercises/exercise13.py
@@ -0,0 +1,6 @@
+# Accept any three strings from one input call
+# by Levance Wamley jr
+
+string1, string2, string3 = input('Enter 3 words: ').split(' ')
+
+print(f'word 1: {string1}, word 2: {string2}, word 3: {string3}')
