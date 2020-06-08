9c692730ca1df6d8c2ca6cd8707d7e001356d517
LevanceWam <lkwamley@fullsail.edu>
2020-06-08 11:22:06 -0400

    completed the exercise 16

---
+++ Exercises/exercise16.py
@@ -0,0 +1,9 @@
+# accept a number from the user and calculate
+# the sum of all the number between 1 and the number given
+# by Levance Wamley jr
+
+n = 10
+count = 0
+for i in range(n+1):
+    count += i
+print(count)
