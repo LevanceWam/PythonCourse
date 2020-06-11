54d29b2c7391a526156af763f00337800bd93855
LevanceWam <lkwamley@fullsail.edu>
2020-06-11 18:30:51 -0400

    Completed exercise 23

---
+++ Exercises/exercise23.py
@@ -0,0 +1,10 @@
+# Create a function that accepts 2 arguments
+# name and age and return the value
+# by Levance Wamley jr
+
+
+def person(name, age):
+    print(f'My name is: {name} and I am {age} years old')
+
+
+person('Jack the Cactus', 3)
